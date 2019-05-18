2f0990c71d8aee6b1bb3f673984cda3a2597dd12
Tomasz Kasprzycki <kasprzyckit@gmail.com>
2019-05-18 11:09:39 +0200

    client init

--- src/main/java/client/GameClient.java
+++ src/main/java/client/GameClient.java
@@ -2,16 +2,15 @@ package client;
 
 import java.io.*;
 import java.net.Socket;
+//import com.jsoniter;
 
 public class GameClient extends Thread{
-    private final String address = "192.168.43.45";
-    private final int port = 50000;
-
-
-//    private ObjectOutputStream outputStream;
-//    private ObjectInputStream inputStream;
+    private final String address = "localhost";
+    private final int port = 9999;
 
     private Socket socket;
+    OutputStreamWriter  outputStream;
+    BufferedReader inputStream;
 
 
     @Override
@@ -19,17 +18,15 @@ public class GameClient extends Thread{
         try {
             System.out.println("ready to conn");
             socket = new Socket(address, port);
-            System.out.println("conn");
-//            outputStream = new ObjectOutputStream(socket.getOutputStream());
-//            inputStream = new ObjectInputStream(socket.getInputStream());
-            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            outputStream = new OutputStreamWriter(socket.getOutputStream());
+            inputStream = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
-            System.out.println("conn2");
-            out.write("aaa");
-            while (true)
-            {
-                String msg = in.readLine();
+            System.out.println("conn");
+            outputStream.write("{\"aa\":34}");
+            outputStream.flush();
+            String msg;
+            while ((msg = inputStream.readLine()) != null) {
+//                JsonIterator.deserialize(msg, int[].class);
                 System.out.println(msg);
             }
         }
