452d18ef29d7e785d08ddc12564a13967e8deaf0
Tomasz Kasprzycki <kasprzyckit@gmail.com>
2019-05-18 13:13:13 +0200

    client init

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,7 +4,7 @@ import game.GameState;
 public class Main {
 
     public static void main(String[] args) {
-        GameClient client = new GameClient();
+        GameClient client = new GameClient("192.168.43.45", 9999);
         client.run();
         try{
         while (true) Thread.sleep(100);
--- src/main/java/client/GameClient.java
+++ src/main/java/client/GameClient.java
@@ -2,7 +2,10 @@ package client;
 
 import java.io.*;
 import java.net.Socket;
-//import com.jsoniter;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
 import com.jsoniter.*;
 
 public class GameClient extends Thread{
@@ -13,31 +16,73 @@ public class GameClient extends Thread{
     OutputStreamWriter  outputStream;
     DataInputStream inputStream;
 
+    private final String serverAdd;
+    private final int serverPort;
+    private String login;
+
+    public GameClient(String addres, int port) {
+        super();
+        this.serverAdd = addres;
+        this.serverPort = port;
+    }
 
     @Override
     public void run() {
         try {
             System.out.println("Game client started");
-            socket = new Socket(address, port);
-            outputStream = new OutputStreamWriter(socket.getOutputStream());
-            inputStream = new DataInputStream(socket.getInputStream());
-
-            System.out.println("Connected to the server");
-            outputStream.write("{\"aa\":34}");
-            outputStream.flush();
-            byte[] buff = new byte[1024];
-            while (inputStream.read(buff) != 0) {
-//                JsonIterator.deserialize(msg, int[].class);
-                Jsoniter json = Jsoniter.parse(buff);
-//                JsonSt
-                System.out.println(json.currentBuffer());
-                Thread.sleep(1000);
-                outputStream.write("{\"aa\":34}");
-                outputStream.flush();
-            }
+            initConnection();
+            login("logggin");
+            listenServer();
         }
         catch (Exception e) {
             e.printStackTrace();
         }
     }
+
+    private void initConnection() throws IOException{
+        socket = new Socket(address, port);//serverAdd, serverPort);
+        outputStream = new OutputStreamWriter(socket.getOutputStream());
+        inputStream = new DataInputStream(socket.getInputStream());
+
+        System.out.println("Connected to the server");
+    }
+
+    private void login(String login) throws IOException{
+        String loginJson =
+                "{" +
+                        "\"login\": \"" + login + "\"" +
+                "}";
+        outputStream.write(loginJson);
+        outputStream.flush();
+    }
+
+    private void listenServer() throws IOException{
+        byte[] buff = new byte[1024];
+        int count;
+        while ((count = inputStream.read(buff)) != 0) {
+            byte[] msg = Arrays.copyOfRange(buff, 0, count);
+
+            GameData gameData = JsonIterator.deserialize(msg, GameData.class);
+            for (PlayerData pd: gameData.play)
+                System.out.println(pd.id + " " + pd.x + " " + pd.y);
+            for (PlanktonData pn: gameData.plan)
+                System.out.println(pn.x + " " + pn.y);
+        }
+    }
+}
+class PlayerData {
+    public int id;
+    public int wght;
+    public int x;
+    public int y;
+    public int xV;
+    public int yV;
+}
+class PlanktonData {
+    public int x;
+    public int y;
+}
+class GameData {
+    public List<PlayerData> play;
+    public List<PlanktonData> plan;
 }
